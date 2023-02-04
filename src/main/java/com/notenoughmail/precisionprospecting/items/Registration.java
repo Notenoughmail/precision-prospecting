d8daa704e9692a0b90af06dfb909f30e18a12b8b
Notenoughmail <78008321+Notenoughmail@users.noreply.github.com>
2023-02-03 21:25:41 -0700

    Channel Casting Integration
    -Add integration for TFC Casting with Channels
    -Bump patch number

--- src/main/java/com/notenoughmail/precisionprospecting/items/Registration.java
+++ src/main/java/com/notenoughmail/precisionprospecting/items/Registration.java
@@ -6,6 +6,8 @@
 
 package com.notenoughmail.precisionprospecting.items;
 
+import com.ljuangbminecraft.tfcchannelcasting.TFCChannelCasting;
+import com.notenoughmail.precisionprospecting.integration.castwchannels.RegisterMoldItem;
 import net.dries007.tfc.common.TFCItemGroup;
 import net.dries007.tfc.common.TFCTags;
 import net.dries007.tfc.common.items.MoldItem;
@@ -14,6 +16,7 @@ import net.dries007.tfc.util.Helpers;
 import net.dries007.tfc.util.Metal;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -32,6 +35,11 @@ public class Registration {
     public static void init() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ITEMS.register(bus);
+        // This doesn't break anything even on a fresh instance without TFCCC, surprisingly, at least on Forge 40.2.0
+        if (ModList.get().isLoaded(TFCChannelCasting.MOD_ID)) {
+            RegisterMoldItem.register();
+        }
+
     }
 
     public static final Map<Metal.Default, RegistryObject<Item>> PROSHAMMERS = Helpers.mapOfKeys(Metal.Default.class, Metal.Default::hasTools, metal ->
@@ -67,10 +75,13 @@ public class Registration {
     public static final RegistryObject<Item>
             UNFIRED_PROSHAMMER_MOLD = ITEMS.register("ceramic/unfired_prospector_hammer_head_mold", () -> new Item(ceramic_properties())),
             FIRED_PROSHAMMER_MOLD = ITEMS.register("ceramic/prospector_hammer_head_mold", () -> new MoldItem(() -> moldProsHammerCapacity.get(), TFCTags.Fluids.USABLE_IN_TOOL_HEAD_MOLD, ceramic_properties())),
+            PROSHAMMER_TABLE_MOLD = ITEMS.register("mold/proshammer_head", () -> new Item(new Item.Properties())),
             UNFIRED_PROSDRILL_MOLD = ITEMS.register("ceramic/unfired_prospector_drill_head_mold", () -> new Item(ceramic_properties())),
             FIRED_PROSDRILL_MOLD = ITEMS.register("ceramic/prospector_drill_head_mold", () -> new MoldItem(() -> moldProsDrillCapacity.get(), TFCTags.Fluids.USABLE_IN_TOOL_HEAD_MOLD, ceramic_properties())),
+            PROSDRILL_TABLE_MOLD = ITEMS.register("mold/prosdrill_head", () -> new Item(new Item.Properties())),
             UNFIRED_MINERAL_PROSPECTOR_MOLD = ITEMS.register("ceramic/unfired_mineral_prospector_head_mold", () -> new Item(ceramic_properties())),
-            FIRED_MINERAL_PROSPECTOR_MOLD = ITEMS.register("ceramic/mineral_prospector_head_mold", () -> new MoldItem(() -> moldMineralProspectorCapacity.get(), TFCTags.Fluids.USABLE_IN_TOOL_HEAD_MOLD, ceramic_properties()))
+            FIRED_MINERAL_PROSPECTOR_MOLD = ITEMS.register("ceramic/mineral_prospector_head_mold", () -> new MoldItem(() -> moldMineralProspectorCapacity.get(), TFCTags.Fluids.USABLE_IN_TOOL_HEAD_MOLD, ceramic_properties())),
+            MINPROS_TABLE_MOLD = ITEMS.register("mold/minpros_head", () -> new Item(new Item.Properties()))
                     ;
     // Big thanks to Alc on Discord for telling me you can just pass in a lambda like this, which makes sense now that I think about it
 
