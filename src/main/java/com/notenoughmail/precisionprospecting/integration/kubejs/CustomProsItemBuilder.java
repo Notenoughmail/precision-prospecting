26d1a9b321f96d934adf3c9dfe728cff52cdd7c5
Notenoughmail <78008321+Notenoughmail@users.noreply.github.com>
2023-02-03 22:20:41 -0700

    Merge AreaHandheldItem into CustomProsItem
    -Remove redundant AreahandheldItemBuilder class
    -Add TFCCC to mods.toml

--- src/main/java/com/notenoughmail/precisionprospecting/integration/kubejs/CustomProsItemBuilder.java
+++ src/main/java/com/notenoughmail/precisionprospecting/integration/kubejs/CustomProsItemBuilder.java
@@ -7,14 +7,62 @@
 package com.notenoughmail.precisionprospecting.integration.kubejs;
 
 import com.notenoughmail.precisionprospecting.items.ProspectorItem;
+import dev.latvian.mods.kubejs.item.custom.HandheldItemBuilder;
 import net.dries007.tfc.common.TFCTags;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
-public class CustomProsItemBuilder extends AreaHandheldItemBuilder {
+public class CustomProsItemBuilder extends HandheldItemBuilder {
+
+    public transient int coolDownBaseline;
+    public transient int primaryRadiusBaseline;
+    public transient int secondaryRadiusBaseline;
+    public transient int displacementBaseline;
+    public transient TagKey<Block> prospectableBaseline;
 
     public CustomProsItemBuilder(ResourceLocation resourceLocation) {
-        super(resourceLocation, 3f, -2.4f, 15, 12, 12, 0, TFCTags.Blocks.PROSPECTABLE);
+        super(resourceLocation, 3f, -2.4f);
+        this.coolDownBaseline = 15;
+        this.primaryRadiusBaseline = 12;
+        this.secondaryRadiusBaseline = 12;
+        this.displacementBaseline = 0;
+        this.prospectableBaseline = TFCTags.Blocks.PROSPECTABLE;
+    }
+
+
+    public CustomProsItemBuilder coolDownBaseline(int i) {
+        coolDownBaseline = i;
+        return this;
+    }
+
+    public CustomProsItemBuilder primaryRadiusBaseline(int i) {
+        primaryRadiusBaseline = i;
+        return this;
+    }
+
+    public CustomProsItemBuilder secondaryRadiusBaseline(int i) {
+        secondaryRadiusBaseline = i;
+        return this;
+    }
+
+    public CustomProsItemBuilder displacementBaseline(int i) {
+        displacementBaseline = i;
+        return this;
+    }
+
+    public CustomProsItemBuilder dimensionsBaseline(int primary, int secondary, int displacement) {
+        primaryRadiusBaseline = primary;
+        secondaryRadiusBaseline = secondary;
+        displacementBaseline = displacement;
+        return this;
+    }
+
+    public CustomProsItemBuilder prospectBlockTag(ResourceLocation tag) {
+        prospectableBaseline = TagKey.create(Registry.BLOCK_REGISTRY, tag);
+        return this;
     }
 
     @Override
