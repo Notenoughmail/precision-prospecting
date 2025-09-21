024c81757210c56eb781eb9ea367be4186325418
Notenoughmail <78008321+Notenoughmail@users.noreply.github.com>
2025-09-21 15:09:00 -0700

    2.0

--- src/main/java/io/github/notenoughmail/precisionprospecting/PrecProsConfig.java
+++ src/main/java/io/github/notenoughmail/precisionprospecting/PrecProsConfig.java
@@ -27,21 +27,21 @@ public class PrecProsConfig {
 
     public PrecProsConfig(ModContainer container) {
         final Builder builder = new Builder();
-        builder.push("prospecting-hammer");
-        prosHammerMoldCapacity = val(builder, "Tank capacity for the prospector hammer mold", "prosHammerMoldCapacity", 200, FluidAlloy.MAX_ALLOY);
-        prosHammerPrimaryRadius = val(builder, "The primary radius of prospector hammers", "prosHammerPrimaryRadius", 6, 25);
-        prosHammerSecondaryRadius = val(builder, "The secondary radius of prospector hammers", "prosHammerSecondaryRadius", 6 , 25);
-        prosHammerDisplacement = val(builder, "The displacement of prospector hammers", "prosHammerDisplacement", 0, 25);
-        builder.pop().push("prospecting-drill");
-        prosDrillMoldCapcaity = val(builder, "Tank capacity for the prospector drill mold", "prosDrillMoldCapacity", 400, FluidAlloy.MAX_ALLOY);
-        prosDrillPrimaryRadius = val(builder, "The primary radius of prospector drills", "prosDrillPrimaryRadius", 3, 25);
-        prosDrillSecondaryRadius = val(builder, "The secondary radius of prospector drills", "prosDrillSecondaryRadius", 12, 25);
-        prosDrillDisplacement = val(builder, "The displacement of prospector drills", "prosDrillDisplacement", 10, 25);
+        builder.push("prospector-hammer");
+        prosHammerMoldCapacity = val(builder, "Tank capacity for the Prospector's Hammer Mold", "prosHammerMoldCapacity", 200, FluidAlloy.MAX_ALLOY);
+        prosHammerPrimaryRadius = val(builder, "The primary radius of Prospector's Hammers", "prosHammerPrimaryRadius", 6, 25);
+        prosHammerSecondaryRadius = val(builder, "The secondary radius of Prospector's Hammers", "prosHammerSecondaryRadius", 6 , 25);
+        prosHammerDisplacement = val(builder, "The displacement of Prospector's Hammers", "prosHammerDisplacement", 0, 25);
+        builder.pop().push("prospector-drill");
+        prosDrillMoldCapcaity = val(builder, "Tank capacity for the Prospector's Drill Mold", "prosDrillMoldCapacity", 400, FluidAlloy.MAX_ALLOY);
+        prosDrillPrimaryRadius = val(builder, "The primary radius of Prospector's Drills", "prosDrillPrimaryRadius", 3, 25);
+        prosDrillSecondaryRadius = val(builder, "The secondary radius of Prospector's Drills", "prosDrillSecondaryRadius", 12, 25);
+        prosDrillDisplacement = val(builder, "The displacement of Prospector's Drills", "prosDrillDisplacement", 10, 25);
         builder.pop().push("mineral-prospector");
-        minProsMoldCapacity = val(builder, "Tank capacity for the mineral prospector mold", "minProsMoldCapacity", 200, FluidAlloy.MAX_ALLOY);
-        minProsPrimaryRadius = val(builder, "The primary radius of mineral prospectors", "minProsPrimaryRadius", 22, 25);
-        minProsSecondaryRadius = val(builder, "The secondary radius of mineral prospectors", "minProsSecondaryRadius", 22, 25);
-        minProsDisplacement = val(builder, "The displacement of mineral prospectors", "minProsDisplacement", 0, 25);
+        minProsMoldCapacity = val(builder, "Tank capacity for the Mineral Prospector Mold", "minProsMoldCapacity", 200, FluidAlloy.MAX_ALLOY);
+        minProsPrimaryRadius = val(builder, "The primary radius of Mineral Prospectors", "minProsPrimaryRadius", 22, 25);
+        minProsSecondaryRadius = val(builder, "The secondary radius of Mineral Prospectors", "minProsSecondaryRadius", 22, 25);
+        minProsDisplacement = val(builder, "The displacement of Mineral Prospectors", "minProsDisplacement", 0, 25);
         builder.pop();
         container.registerConfig(ModConfig.Type.SERVER, builder.build(), "precision-prospecting.toml");
     }
